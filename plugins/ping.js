946247d588677e2d23d0041e9f74324f55cd9992
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-03-26 19:33:13 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,8 +32,20 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *KHAN-MD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *Felix ki SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
+        await conn.sendMessage(from, {
+            text,
+            contextInfo: {
+                mentionedJid: [sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363354023106228@newsletter',
+                    newsletterName: "JawadTechX",
+                    serverMessageId: 143
+                }
+            }
         }, { quoted: mek });
 
     } catch (e) {
